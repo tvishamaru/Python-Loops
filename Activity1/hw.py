55231586fae861c38ce9036d5081f13afb7fd76e
tvishamaru <tvisha.maru@gmail.com>
2026-02-24 17:59:22 +0530

    hw

---
+++ Activity1/hw.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age: "))
+if age<10 :
+    print("Your age is less than 10.")
+else:
+    if(age<20):
+        print("Your age is between 10 and 20.")
+    else:
+        print("Your age is greater than 20.")
