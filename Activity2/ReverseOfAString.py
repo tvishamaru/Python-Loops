7731eb4ee7aa2882f4612c868fcadbb26e8900ce
tvishamaru <tvisha.maru@gmail.com>
2026-02-26 17:07:44 +0530

    initial

---
+++ Activity2/ReverseOfAString.py
@@ -0,0 +1,8 @@
+s1=input("Enter a word:")
+s2=('')
+
+for i in s1:
+    s2=i+s2
+
+print("\n The orignal string is", s1)
+print("The reversed string is", s2)
---
+++ Activity2/ReverseOrder.py
@@ -0,0 +1,5 @@
+n=int(input("Enter a value of n:"))
+
+print("The numbers from {0} to {1} is:".format(n,1))
+for i in range(n,0,-1):
+    print(i)
