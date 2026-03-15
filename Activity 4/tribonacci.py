088b1868127236a483008a75016d6790f9f639a3
tvishamaru <tvisha.maru@gmail.com>
2026-03-15 17:23:03 +0530

    hnbhj

--- Activity 4/tribonacci.py
+++ Activity 4/tribonacci.py
@@ -1,5 +1,5 @@
-num_fib=int(input("Enter the number of fibonacci numbers you want:"))
-print(num_fib)
+num_trib=int(input("Enter the number of tribonacci numbers you want:"))
+print(num_trib)
 
 num1=1
 print(num1)
@@ -8,7 +8,7 @@ print(num2)
 num3=2
 print(num3)
 
-for num in range(num_fib-3):
+for num in range(num_trib-3):
     num4=num1+num2+num3
     print(num4)
     num1=num2
